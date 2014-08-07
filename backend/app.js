1cd8b64249028f909479dd97d1101986918894bf
paul <william.paul@inist.fr>
2014-08-07 09:58:26 +0200

    ./frontend/ > ./

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var express = require('express');
-var app = express();
-
-var server = app.listen(28081, function() {
-  console.log('Listening on port %d', server.address().port);
-});
