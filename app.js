aa6994d4afcd4c2e405e8c81ec5a069603416d75
paul <william.paul@inist.fr>
2016-06-01 16:07:37 +0200

    Serving static par express. Nettoyage des ficheirs init d'express

--- app.js
+++ app.js
@@ -10,32 +10,30 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
+// Handler d'erreur
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
+//Middleware général
+app.use(function(req, res, next) {
+  res.charset = 'utf-8';
+  next();
+});
 
-// development error handler
-// will print stacktrace
+
+// Dèv handler
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -46,8 +44,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// Prod handler
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
